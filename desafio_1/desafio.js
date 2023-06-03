a1cd8d95383d49a17a477326d96f47377dd3bd23
Eric Moniz <ericmoniz@outlook.com.br>
2023-06-03 20:23:33 -0300

    Desafio 1

---
+++ desafio_1/desafio.js
@@ -0,0 +1,6 @@
+/* criar uma função que aceita dois argumentos do tipo number e imprima na tela
+a soma desse resultado */
+function sum(n1, n2) {
+    return n1 + n2;
+}
+console.log(sum(1, 4));
---
+++ desafio_1/desafio.ts
@@ -0,0 +1,8 @@
+/* criar uma função que aceita dois argumentos do tipo number e imprima na tela
+a soma desse resultado */
+
+function sum(n1: number, n2: number) {
+	return n1 + n2;
+}
+
+console.log(sum(1, 4));
