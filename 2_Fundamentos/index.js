e9095668d7e863f3ca7c9b98b2f0fe9d23dec807
Eric Moniz <ericmoniz@outlook.com.br>
2023-06-03 20:42:37 -0300

    O tipo number

---
+++ 2_Fundamentos/index.js
@@ -0,0 +1,5 @@
+// 1 - numbers
+var x = 10;
+console.log(x);
+var y = 15.8576;
+console.log(y.toPrecision(4));
---
+++ 2_Fundamentos/index.ts
@@ -0,0 +1,7 @@
+// 1 - numbers
+let x: number = 10;
+
+console.log(x);
+
+const y: number = 15.8576;
+console.log(y.toPrecision(4));
