d0556425096c8842990ee2b2c1e42431dc8ead1b
Eric Moniz <ericmoniz@outlook.com.br>
2023-06-03 21:20:15 -0300

    Type inference e Type annotation

--- 2_Fundamentos/index.ts
+++ 2_Fundamentos/index.ts
@@ -1,4 +1,5 @@
-// 1 - numbers
+//* 1 - numbers
+
 let x: number = 10;
 
 console.log(x);
@@ -6,7 +7,8 @@ console.log(x);
 const y: number = 15.8576;
 console.log(y.toPrecision(4));
 
-// 2 - string
+//* 2 - string
+
 const firstName: string = 'Eric';
 console.log(firstName.toUpperCase());
 
@@ -25,3 +27,9 @@ console.log(typeof a);
 
 a = true;
 console.log(a);
+
+//* 4 - Inference e Anotation
+
+let ann: string = 'Teste'; // anotation, nós que definimos o tipo, nessa caso uma string
+
+let inf = 'Teste'; // inference, o typescript assume que é uma string
