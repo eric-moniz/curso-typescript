d036419c231a98fad8d652b7d2eafca7a0d9adc3
Eric Moniz <ericmoniz@outlook.com.br>
2023-06-04 10:54:45 -0300

    Arrays

---
+++ 3_Avançando_em_tipos/index.js
@@ -0,0 +1,6 @@
+"use strict";
+//* 1. Arrays
+let numbers = [1, 2, 3];
+numbers.push(10);
+console.log(numbers[2]);
+const nomes = ['Eric', 'Joâo'];
---
+++ 3_Avançando_em_tipos/index.ts
@@ -0,0 +1,9 @@
+//* 1. Arrays
+
+let numbers: number[] = [1, 2, 3];
+
+numbers.push(10);
+
+console.log(numbers[2]);
+
+const nomes: string[] = ['Eric', 'Joâo'];
