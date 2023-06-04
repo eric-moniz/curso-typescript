cdb01383c82262bc48804388f5e46dd7c6320950
Eric Moniz <ericmoniz@outlook.com.br>
2023-06-04 11:41:39 -0300

    Outra sintaxe de arrays

--- 3_Avançando_em_tipos/index.js
+++ 3_Avançando_em_tipos/index.js
@@ -1,6 +1,11 @@
 "use strict";
-//* 1. Arrays
+//* 1. Arrays - mais utilizada essa sintaxe
 let numbers = [1, 2, 3];
 numbers.push(10);
 console.log(numbers[2]);
 const nomes = ['Eric', 'Joâo'];
+//* 2. Outras sintaxes de arrays - não muito usado essa sintaxe
+const nums = [100, 200];
+nums.push(300);
+console.log(nums);
+console.log(nums[2]);
--- 3_Avançando_em_tipos/index.ts
+++ 3_Avançando_em_tipos/index.ts
@@ -1,4 +1,4 @@
-//* 1. Arrays
+//* 1. Arrays - mais utilizada essa sintaxe
 
 let numbers: number[] = [1, 2, 3];
 
@@ -7,3 +7,10 @@ numbers.push(10);
 console.log(numbers[2]);
 
 const nomes: string[] = ['Eric', 'Joâo'];
+
+//* 2. Outras sintaxes de arrays - não muito usado essa sintaxe
+
+const nums: Array<number> = [100, 200];
+nums.push(300);
+console.log(nums);
+console.log(nums[2]);
