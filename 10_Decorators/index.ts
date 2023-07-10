d71cc544d4b8972bf2861612014485a26b5e3a18
eric-moniz <ericmoniz@outlook.com>
2023-07-10 20:27:47 -0300

    Sobre os decorators

---
+++ 10_Decorators/index.ts
@@ -0,0 +1,7 @@
+//* O que são os decorators?
+/*
+    Decorators podem adicionar funcionalidades extras a classes e funções;
+    Basicamente criamos novas funções, que são adicionadas a partir de um '@nome';
+    Esta função será chamada assim que o item que foi definido o decorator for executado;
+    Para habilitar precisamos adicionar uma configuração no tsconfig.json;
+*/
