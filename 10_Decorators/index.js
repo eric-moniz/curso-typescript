f751bae05caf8a46c0678fb5ac31e2c214ee9101
eric-moniz <ericmoniz@outlook.com>
2023-07-13 20:29:04 -0300

    Exemplo real com Method decorator

--- 10_Decorators/index.js
+++ 10_Decorators/index.js
@@ -223,4 +223,38 @@ console.log(newBook.createdAt);
 /////////////////////////////////////////////////////////////////////////////////////////
 /*
  * 8. Exemplo real com Method decorator
+  Com o Method Decorator podemos alterar a execução dos métodos;
+  Neste exemplo vamos verificar se um usuário pode ou não fazer uma alteração no sistema;
+  A alteração seria o método a ser executado
  */
+function checkIfUserPosted() {
+    return function (target, key, descriptor) {
+        const childFunction = descriptor.value;
+        // console.log(childFunction);
+        descriptor.value = function (...args) {
+            if (args[1] === true) {
+                console.log('Usuário já postou!');
+                return null;
+            }
+            else {
+                return childFunction.apply(this, args);
+            }
+        };
+        return descriptor;
+    };
+}
+class Post {
+    constructor() {
+        this.alreadyPosted = false;
+    }
+    post(content, alreadyPosted) {
+        this.alreadyPosted = true;
+        console.log(`Post do usuário: ${content}`);
+    }
+}
+__decorate([
+    checkIfUserPosted()
+], Post.prototype, "post", null);
+const newPost = new Post();
+newPost.post('Meu primeiro post!', newPost.alreadyPosted);
+newPost.post('Meu segundo post!', newPost.alreadyPosted);
