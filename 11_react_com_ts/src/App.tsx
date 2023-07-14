b7def87a3ed2f85527250838dc5d3050b0e60725
eric-moniz <ericmoniz@outlook.com>
2023-07-14 20:27:59 -0300

    Funções em componentes

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 function App() {
   // 1. Variaveis
-  const name: string = 'Curso TS';
+  const name: string = 'Eric';
   const ano: number = 2023;
   const isWorking: boolean = true;
+
+  // 2. funções
+  const userGreeting = (name: string): string => {
+    return `Olá, ${name}!`;
+  };
 
   return (
     <div className="App">
@@ -12,6 +17,7 @@ function App() {
       <h2>{name}</h2>
       <p>{ano}</p>
       {isWorking && <p>Está trabalhando!</p>}
+      <h3>{userGreeting(name)}</h3>
     </div>
   );
 }
