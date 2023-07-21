422b6535a0b1dba1075f30bf314c50bfa3fa0cd6
eric-moniz <ericmoniz@outlook.com.br>
2023-07-21 12:27:53 -0300

    Removendo tarefas

--- 12_todo_react/src/App.tsx
+++ 12_todo_react/src/App.tsx
@@ -12,6 +12,14 @@ import { ITask } from './interfaces/Task';
 function App() {
   const [taskList, setTaskList] = useState<ITask[]>([]);
 
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter(task =>{
+        return task.id !== id
+      })
+    )
+  }
+
   return (
     <div className={styles.main}>
       <Header />
@@ -28,7 +36,7 @@ function App() {
 
         <div>
           <h2>Suas tarefas</h2>
-          <TaskList taskList={taskList} />
+          <TaskList taskList={taskList} handleDelete={deleteTask}/>
         </div>
       </main>
 
