cdd9e16997c6cfcdff57526e0c4c5cc854d5f0ed
eric-moniz <ericmoniz@outlook.com.br>
2023-07-21 06:40:50 -0300

    Exibindo listas de tarefas

--- 12_todo_react/src/components/TaskList.tsx
+++ 12_todo_react/src/components/TaskList.tsx
@@ -1,9 +1,24 @@
-interface Props {}
+import { ITask } from '../interfaces/Task';
 
-const TaskList = (props: Props) => {
+import styles from './TaskList.module.css';
+
+interface Props {
+  taskList: ITask[];
+}
+
+const TaskList = ({ taskList }: Props) => {
   return (
     <>
-      <p>TaskList</p>
+      {taskList.length > 0 ? (
+        taskList.map(task => (
+          <div key={task.id}>
+            <p>{task.title}</p>
+            <p>{task.difficulty}</p>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas</p>
+      )}
     </>
   );
 };
