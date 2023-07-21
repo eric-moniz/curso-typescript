65fb370a4f6831a26b303c0d8e1a41cf25e0616e
eric-moniz <ericmoniz@outlook.com.br>
2023-07-21 12:44:41 -0300

    Criando modal

--- 12_todo_react/src/App.tsx
+++ 12_todo_react/src/App.tsx
@@ -1,46 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import styles from './App.module.css';
+import styles from "./App.module.css";
 
-import Footer from './components/Footer';
-import Header from './components/Header';
-import TaskForm from './components/TaskForm';
-import TaskList from './components/TaskList';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 
-import { ITask } from './interfaces/Task';
+import { ITask } from "./interfaces/Task";
+import Modal from "./components/Modal";
 
 function App() {
   const [taskList, setTaskList] = useState<ITask[]>([]);
 
   const deleteTask = (id: number) => {
     setTaskList(
-      taskList.filter(task =>{
-        return task.id !== id
+      taskList.filter((task) => {
+        return task.id !== id;
       })
-    )
-  }
+    );
+  };
 
   return (
-    <div className={styles.main}>
+    <div>
+      <Modal />
       <Header />
-
-      <main className={styles.main}>
-        <div>
-          <h2>O que você vai fazer?</h2>
-          <TaskForm
-            btnText={'Criar tarefa'}
-            taskList={taskList}
-            setTaskList={setTaskList}
-          />
-        </div>
-
-        <div>
-          <h2>Suas tarefas</h2>
-          <TaskList taskList={taskList} handleDelete={deleteTask}/>
-        </div>
-      </main>
-
-      <Footer />
+      <div className={styles.main}>
+        <main className={styles.main}>
+          <div>
+            <h2>O que você vai fazer?</h2>
+            <TaskForm
+              btnText={"Criar tarefa"}
+              taskList={taskList}
+              setTaskList={setTaskList}
+            />
+          </div>
+          <div>
+            <h2>Suas tarefas</h2>
+            <TaskList taskList={taskList} handleDelete={deleteTask} />
+          </div>
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ 12_todo_react/src/components/Modal.tsx
@@ -0,0 +1,16 @@
+import styles from "./Modal.module.css";
+
+interface Props {}
+
+const Modal = (props: Props) => {
+  return (
+    <div id="modal">
+      <div></div>
+      <div>
+        <h2>Texto Modal</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
