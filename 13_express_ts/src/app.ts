2f84b4ccf77b479c6fa359b16ce117d21f6a9eae
eric-moniz <ericmoniz@outlook.com.br>
2023-07-24 13:02:05 -0300

    Iniciando o Express

--- 13_express_ts/src/app.ts
+++ 13_express_ts/src/app.ts
@@ -1,3 +1,16 @@
 // 1. - iniciando o projeto
-console.log("Express + TS");
+// console.log("Express + TS");
 
+//* 2. Iniciar express
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("Hello Express");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Aplicação express + TS funcionando na porta ${PORT}`);
+});
