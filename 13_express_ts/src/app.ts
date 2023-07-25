af5a1709559d0e25eff304f30e3cfba3910cdd46
eric-moniz <ericmoniz@outlook.com.br>
2023-07-25 06:57:59 -0300

    Rota para qualquer verbo

--- 13_express_ts/src/app.ts
+++ 13_express_ts/src/app.ts
@@ -17,6 +17,19 @@ app.post("/api/product", (req, res) => {
   return res.send("Produto adicionado!");
 });
 
+app.all("/api/product/check", (req, res) => {
+  switch (req.method) {
+    case "POST":
+      return res.send("Inseriu algum registro");
+
+    case "GET":
+      return res.send("Leu algum registro");
+
+    default:
+      return res.send("Não podemos realizar esta operação!");
+  }
+});
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Aplicação express + TS funcionando na porta ${PORT}`);
