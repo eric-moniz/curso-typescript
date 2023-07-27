0c4ed0a8a2779065220a640fa21d7c87a7327f41
eric-moniz <ericmoniz@outlook.com.br>
2023-07-27 12:26:01 -0300

    Integrando o express

--- 14_api_restful_ts/src/app.ts
+++ 14_api_restful_ts/src/app.ts
@@ -1,1 +1,13 @@
-console.log("Server start");
+import express from "express";
+import config from "config";
+
+const app = express();
+
+// JSON middleware
+app.use(express.json());
+
+// app port
+const port = config.get<number>("port");
+app.listen(port, async () => {
+  console.log(`Aplicação está funcionando na porta: ${port}`);
+});
