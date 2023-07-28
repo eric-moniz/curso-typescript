ba18f00612c9f17a2602e80be95a5236b623bb2f
eric-moniz <ericmoniz@outlook.com.br>
2023-07-28 06:41:16 -0300

    Criando variáveis de ambiente

--- 14_api_restful_ts/config/default.ts
+++ 14_api_restful_ts/config/default.ts
@@ -1,5 +1,7 @@
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
 export default {
   port: 3000,
-  dbUri:
-    "mongodb+srv://ericmonizem:UY2oQR9nhLGgEXcQ@cluster0.y4vjhn0.mongodb.net/",
+  dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.y4vjhn0.mongodb.net/`,
 };
