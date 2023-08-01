cb471d2725632dccf950a148cc116b9d2bf840e2
eric-moniz <ericmoniz@outlook.com.br>
2023-08-01 06:43:35 -0300

    Resgatando filme pelo ID

--- 14_api_restful_ts/src/router.ts
+++ 14_api_restful_ts/src/router.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { createMovie } from "./controllers/movieControler";
+import { createMovie, findMovieById } from "./controllers/movieControler";
 
 // middleware(s) de validação
 import { validate } from "./middleware/handleValidation";
@@ -11,4 +11,5 @@ export default router
   .get("/test", (req: Request, res: Response) => {
     res.status(200);
   })
-  .post("/movie", movieCreateValidation(), validate, createMovie);
+  .post("/movie", movieCreateValidation(), validate, createMovie)
+  .get('/movie/:id', findMovieById)
